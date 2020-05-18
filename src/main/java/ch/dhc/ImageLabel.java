2a7b5253ed1e68cd015997165efb778b2b13b6ae
droduit <daniel@roduit.com>
2020-05-18 19:30:09 +0200

    Add TODO

--- src/main/java/ch/dhc/ImageLabel.java
+++ src/main/java/ch/dhc/ImageLabel.java
@@ -7,6 +7,8 @@ public class ImageLabel extends JLabel {
     public ImageLabel(String imagePath) {
         ImageIcon img = new ImageIcon(imagePath);
         setIcon(img);
+
+        // TODO: Mettre une taille maximale pour l'affichage de l'icône (60px x 60px comme Cathy)
     }
 
 }

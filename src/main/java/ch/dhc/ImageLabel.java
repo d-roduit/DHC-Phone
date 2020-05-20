23b9a133cfb370d8277672c20ef39dc65db8f674
g.cathy <cathy.gay@students.hevs.ch>
2020-05-20 23:56:54 +0200

    Add javadoc to CircleButton, DragPanel and ImageLabel classes.

--- src/main/java/ch/dhc/ImageLabel.java
+++ src/main/java/ch/dhc/ImageLabel.java
@@ -2,8 +2,24 @@ package ch.dhc;
 
 import javax.swing.*;
 
+/**
+ * <b>ImageLabel is the class that creates an image in a JLabel.</b>
+ *
+ * @see JLabel
+ *
+ * @author Cathy Gay
+ * @author Daniel Roduit
+ */
 public class ImageLabel extends JLabel {
 
+    /**
+     * ImageLabel Constructor.
+     *
+     * @param imagePath
+     *          Path of the image.
+     *
+     * @see ImageIcon
+     */
     public ImageLabel(String imagePath) {
         ImageIcon img = new ImageIcon(imagePath);
         setIcon(img);

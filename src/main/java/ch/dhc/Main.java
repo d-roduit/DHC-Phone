0a329796128f50122abedd0e76927bd26f63da83
droduit <daniel@roduit.com>
2020-05-27 15:56:44 +0200

    Add Flatlaf Look' n Feel

--- src/main/java/ch/dhc/Main.java
+++ src/main/java/ch/dhc/Main.java
@@ -1,8 +1,17 @@
 package ch.dhc;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        FlatDarculaLaf.install();
+
+        javax.swing.UIManager.put("defaultFont", new Font("Dialog", Font.BOLD, 12));
+
         SmartphoneFrame smartphoneFrame = new SmartphoneFrame();
 
         UIManager uiManager = UIManager.getInstance();

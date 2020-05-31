7f21a00dcb0c829c19c33aa63c31d1a304c649a8
g.cathy <cathy.gay@students.hevs.ch>
2020-05-31 21:12:51 +0200

    Add FolderPanel view and fix FolderListPanel

---
+++ src/main/java/applications/Notes/FolderPanel.java
@@ -0,0 +1,150 @@
+package applications.Notes;
+
+import jiconfont.icons.font_awesome.FontAwesome;
+import jiconfont.icons.google_material_design_icons.GoogleMaterialDesignIcons;
+import jiconfont.swing.IconFontSwing;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * <b>FolderPanel is the class that represents the notes list of a folder.</b>
+ *
+ * @see JPanel
+ *
+ * @author Cathy Gay
+ */
+public class FolderPanel extends JPanel {
+
+    private Color mainTextColor = Color.WHITE;
+    private Color secondaryTextColor = new Color(217, 169, 25);
+
+    /**
+     * FolderPanel constructor.
+     */
+    public FolderPanel() {
+        setLayout(new BorderLayout());
+        setBackground(Color.BLACK);
+
+        add(createTopLanePanel(), BorderLayout.NORTH);
+        add(createNotesListScrollPanel(), BorderLayout.CENTER);
+        add(createBotLanePanel(), BorderLayout.SOUTH);
+    }
+
+    private JPanel createBotLanePanel() {
+        JPanel panel = new JPanel();
+        panel.setOpaque(false);
+        int nbNotes = createNotesListPanel().getComponentCount() / 2;
+        JLabel nbNotesLabel = new JLabel(nbNotes +" notes");
+        nbNotesLabel.setForeground(mainTextColor);
+
+        panel.add(nbNotesLabel);
+
+        return panel;
+    }
+
+    private JPanel createTopLanePanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setOpaque(false);
+
+        Icon goBackIcon = IconFontSwing.buildIcon(FontAwesome.ANGLE_LEFT, 22, secondaryTextColor);
+
+        JButton returnToFolderList = new JButton("Folders");
+        returnToFolderList.setIcon(goBackIcon);
+        returnToFolderList.setContentAreaFilled(false);
+        returnToFolderList.setFocusPainted(false);
+        returnToFolderList.setBorderPainted(false);
+        returnToFolderList.setForeground(secondaryTextColor);
+        returnToFolderList.setFont(new Font("Arial", Font.BOLD, 17));
+        returnToFolderList.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        returnToFolderList.setToolTipText("Go back to folders");
+
+        JLabel titleLabel = new JLabel("Notes");
+        titleLabel.setForeground(mainTextColor);
+        titleLabel.setHorizontalAlignment(JLabel.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 25));
+
+        Icon addNoteIcon = IconFontSwing.buildIcon(GoogleMaterialDesignIcons.ADD, 28, secondaryTextColor);
+
+        Icon deleteFolderIcon = IconFontSwing.buildIcon(FontAwesome.TRASH_O, 24, secondaryTextColor);
+
+        JButton addNoteButton = new JButton(addNoteIcon);
+        addNoteButton.setFocusPainted(false);
+        addNoteButton.setBorderPainted(false);
+        addNoteButton.setContentAreaFilled(false);
+        addNoteButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        addNoteButton.setToolTipText("Add a note");
+
+        JButton deleteFolderButton = new JButton(deleteFolderIcon);
+        deleteFolderButton.setFocusPainted(false);
+        deleteFolderButton.setBorderPainted(false);
+        deleteFolderButton.setContentAreaFilled(false);
+        deleteFolderButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        deleteFolderButton.setToolTipText("Delete this folder");
+
+        JPanel eastPanel = new JPanel();
+        eastPanel.setOpaque(false);
+
+        eastPanel.add(addNoteButton);
+        eastPanel.add(deleteFolderButton);
+
+        panel.add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.NORTH);
+        panel.add(returnToFolderList, BorderLayout.WEST);
+        panel.add(titleLabel, BorderLayout.CENTER);
+        panel.add(eastPanel, BorderLayout.EAST);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)), BorderLayout.SOUTH);
+
+        return panel;
+    }
+
+    private JScrollPane createNotesListScrollPanel() {
+        JScrollPane scrollPane = new JScrollPane(createNotesListPanel(), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setOpaque(false);
+        scrollPane.getViewport().setOpaque(false);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.getVerticalScrollBar().setUnitIncrement(10); //change the scrolling speed
+
+        return scrollPane;
+    }
+
+    private JPanel createNotesListPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setOpaque(false);
+
+        for(int i = 0; i < 4; i++) {
+            panel.add(createNotePanel("Homework"));
+            JSeparator separator = new JSeparator();
+            separator.setPreferredSize(new Dimension(342, 2));
+            separator.setMaximumSize(new Dimension(separator.getPreferredSize()));
+            separator.setBackground(new Color(49, 49, 49));
+            separator.setForeground(new Color(49, 49, 49));
+            panel.add(separator);
+        }
+
+        return panel;
+    }
+
+    private JPanel createNotePanel(String name) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setOpaque(false);
+        panel.setPreferredSize(new Dimension(320, 40));
+        panel.setMaximumSize(new Dimension(panel.getPreferredSize()));
+
+        JLabel noteTitle = new JLabel(name);
+        noteTitle.setForeground(mainTextColor);
+
+        Icon goToNoteIcon = IconFontSwing.buildIcon(FontAwesome.ANGLE_RIGHT, 20, mainTextColor);
+
+        JLabel goToNoteLabel = new JLabel(goToNoteIcon);
+
+        panel.add(Box.createRigidArea(new Dimension(10, 0)), BorderLayout.WEST);
+        panel.add(noteTitle, BorderLayout.CENTER);
+        panel.add(goToNoteLabel, BorderLayout.EAST);
+
+        return panel;
+    }
+
+}

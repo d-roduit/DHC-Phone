ca8841dbab972fda92bf6d928a43c0680cb68b67
Henrick Neads <henrick.neads@students.hevs.ch>
2020-06-03 14:47:41 +0200

    Create test class for Contact

---
+++ src/test/java/applications/Contacts/ContactListTest.java
@@ -0,0 +1,20 @@
+package applications.Contacts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Ceci est la classe test
+ */
+
+class ContactListTest {
+
+  /*  @Test
+    void testAddContact(Contact contact) {
+        Contact ben = new Contact("Ben","Silva","London","0786542334","Bensilva@gmail.com");
+        assertEquals(contact.getFirstName(),Contact.class);
+    }
+
+   */
+}

dee3fdfb5a29efe51acbbb213e3f667718529aac
g.cathy <cathy.gay@students.hevs.ch>
2020-06-08 21:08:29 +0200

    Add an addNote listener. Add an update method for the view.

--- src/main/java/applications/Notes/controllers/FolderController.java
+++ src/main/java/applications/Notes/controllers/FolderController.java
@@ -1,6 +1,7 @@
 package applications.Notes.controllers;
 
 import applications.Notes.models.FolderModel;
+import applications.Notes.models.NoteModel;
 import applications.Notes.views.FolderListView;
 import applications.Notes.views.FolderView;
 import ch.dhc.Configuration;
@@ -29,22 +30,54 @@ public class FolderController {
     private void initListeners() {
 
         //Listener for adding a note.
-        folderView.getAddNoteButton().addActionListener(e -> folderModel.addNote());
+        folderView.getAddNoteButton().addActionListener(e -> {
+//            folderModel.addNote();
+            createNewNote();
+            updateFolderView(folderModel);
+        });
 
         //Listener for deleting a folder.
         folderView.getDeleteFolderButton().addActionListener(e -> deleteFolder());
 
         //Listener for going back to the FolderList.
         folderView.getReturnToFolderListButton().addActionListener(e ->
-                folderListController.getMain().getCardLayout().show(folderListController.getMain(), String.valueOf(folderListController.getFolderListView().hashCode())));
+                folderListController.updateFolderListView(folderListController.getFolderListModel()));
 
+    }
+
+    private void createNewNote() {
+
+        String newNoteTitle = JOptionPane.showInputDialog(
+                folderListController.getMain(),
+                "New Note Title :",
+                "Create New Note",
+                JOptionPane.QUESTION_MESSAGE
+        );
+
+        try {
+            if(newNoteTitle != null) {
+                Configuration configuration = Configuration.getInstance();
+                ObjectMapper mapper = new ObjectMapper();
+                ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+
+                File folderFile = new File(configuration.getNotesFolderPath() + "folders.json");
+
+                folderModel.addNote(newNoteTitle);
+
+                List<FolderModel> folderModels = folderListController.getFolderListModel().getFolderModels();
+
+                writer.writeValue(folderFile, folderModels);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void deleteFolder() {
 
         int option = JOptionPane.showConfirmDialog(
                 folderListController.getMain(),
-                "Are you sure you want to delete " + folderModel.getFolderTitle() + " folder ?",
+                "Are you sure you want to delete \"" + folderModel.getFolderTitle() + "\" folder ? \nThis will delete all its notes as well.",
                 "Delete Folder",
                 JOptionPane.YES_NO_OPTION
         );
@@ -62,20 +95,21 @@ public class FolderController {
 
                 writer.writeValue(folderFile, folderModels);
 
-                folderListController.getMain().remove(folderListController.getFolderListView());
-                FolderListView folderListView = folderListController.getFolderListView();
-                folderListView = new FolderListView(folderListController.getFolderListModel());
-                folderListController.getMain().add(folderListView, String.valueOf(folderListView.hashCode()));
-                folderListController.getMain().revalidate();
-                folderListController.getMain().repaint();
-                folderListController.getMain().getCardLayout().show(folderListController.getMain(), String.valueOf(folderListView.hashCode()));
-                folderListController = new FolderListController(folderListController.getMain(), folderListController.getFolderListModel(), folderListView);
+                folderListController.updateFolderListView(folderListController.getFolderListModel());
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
-
+    public void updateFolderView(FolderModel folderModel) {
+        folderListController.getMain().remove(folderView);
+        folderView = new FolderView(folderModel);
+        folderListController.getMain().add(folderView, String.valueOf(folderView.hashCode()));
+        folderListController.getMain().revalidate();
+        folderListController.getMain().repaint();
+        folderListController.getMain().getCardLayout().show(folderListController.getMain(), String.valueOf(folderView.hashCode()));
+        initListeners();
     }
 }

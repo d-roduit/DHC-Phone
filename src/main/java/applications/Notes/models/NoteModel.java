7b4cc863877cc9b4f6d71b3ef32640d88b4fbca0
g.cathy <cathy.gay@students.hevs.ch>
2020-06-10 15:13:50 +0200

    Add javadoc to the models in the note app.

--- src/main/java/applications/Notes/models/NoteModel.java
+++ src/main/java/applications/Notes/models/NoteModel.java
@@ -1,17 +1,50 @@
 package applications.Notes.models;
 
+/**
+ * <b>NoteModel is the class that represents a note with its properties and its methods.</b>
+ *
+ * @author Cathy Gay
+ */
 public class NoteModel {
 
+    /**
+     * The note title.
+     */
     private String noteTitle;
+
+    /**
+     * The note creation date.
+     */
     private String noteCreationDate;
+
+    /**
+     * The note content.
+     */
     private String content;
 //    private String [] contacts;
 //    private String [] pictures;
 
+    /**
+     * NoteModel empty constructor.
+     */
     public NoteModel() {
 
     }
 
+    /**
+     * NoteModel constructor.
+     *
+     * @param noteTitle
+     *              The note title.
+     * @param noteCreationDate
+     *              The note creation date.
+     * @param content
+     *              The note content.
+     *
+     * @see NoteModel#setNoteTitle(String)
+     * @see NoteModel#setNoteCreationDate(String)
+     * @see NoteModel#setContent(String)
+     */
     public NoteModel(String noteTitle, String noteCreationDate, String content) {
         setNoteTitle(noteTitle);
         setNoteCreationDate(noteCreationDate);
@@ -32,26 +65,59 @@ public class NoteModel {
 
     }
 
+    /**
+     * Returns the note title.
+     *
+     * @return The note title.
+     */
     public String getNoteTitle() {
         return noteTitle;
     }
 
+    /**
+     * Sets the note title.
+     *
+     * @param title
+     *              The note title.
+     */
     public void setNoteTitle(String title) {
         this.noteTitle = title;
     }
 
+    /**
+     * Returns the note creation date.
+     *
+     * @return The note creation date.
+     */
     public String getNoteCreationDate() {
         return noteCreationDate;
     }
 
+    /**
+     * Sets the note creation date.
+     *
+     * @param creationDate
+     *              The note creation date.
+     */
     public void setNoteCreationDate(String creationDate) {
         this.noteCreationDate = creationDate;
     }
 
+    /**
+     * Returns the note content.
+     *
+     * @return The note content.
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Sets the note content.
+     *
+     * @param note
+     *              The note content.
+     */
     public void setContent(String note) {
         this.content = note;
     }
